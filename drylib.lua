0842548171ca75acf9216bba9b9a7fc0a00875c3
Arto Bendiken <arto@dryproject.org>
2017-09-03 15:24:21 +0200

    Imported skeletal drylib.lua and test.lua files.

---
+++ drylib.lua
@@ -0,0 +1,9 @@
+-- This is free and unencumbered software released into the public domain.
+
+--- DRYlib for Lua
+-- @module dry
+local dry = {}
+
+--------------------------------------------------------------------------------
+
+return dry
---
+++ test.lua
@@ -0,0 +1,6 @@
+#!/usr/bin/env lua5.3
+-- This is free and unencumbered software released into the public domain.
+
+local dry = require("drylib")
+
+print("OK")
